955e2f96f261fc3fb05fc18244cacd5b89770705
Ajinkyaa2004 <dhumalajinkya2004@gmail.com>
2024-09-02 20:43:31 +0530

    First Project

---
+++ Index.js
@@ -0,0 +1,7 @@
+// const bar=document.getElementById('bar');
+// const navbar=document.getElementById('navbar');
+//  if(bar){
+//     bar.addEventListener('click',()=> {
+//         navbar.classList.add('active');
+//     })
+//  }
